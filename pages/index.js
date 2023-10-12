d643eca7caf2f7a785c24141cdefb072b7fae5a2
Richard Burd <richardburd@yahoo.com>
2023-10-12 22:37:25 +0000

    fresh reinstall w/no Next.js 'App Router' & all modules

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <div className="m-8 text-2xl lg:text-6xl text-stone-700">
+      The RAM website is comming soon!
+    </div>
+  );
+}
