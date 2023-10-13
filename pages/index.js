7e3824b663f0e689ddcaf9b01004496b76f836c2
Richard Burd <richardburd@yahoo.com>
2023-10-13 01:30:13 +0000

    CI/CD Test

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div>
       <div className="m-8 text-2xl lg:text-6xl text-stone-700">
-        The RAM website is comming soon!
+        ♻️ The RAM website is comming soon!
       </div>
-      <div className="m-8 text-1xl lg:text-1xl text-stone-700">
+      <div className="m-8 text-1xl lg:text-2xl text-stone-700">
         Richard got everything working in the backend, now he can just build out
-        the website content
+        the website content. 😁
       </div>
     </div>
   );
