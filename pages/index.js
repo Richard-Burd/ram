dbb60a99756ea976f32aea1035d6ccd2ca24a6f9
Richard Burd <richardburd@yahoo.com>
2023-10-13 01:24:47 +0000

    subtitle added

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,14 @@ import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
-    <div className="m-8 text-2xl lg:text-6xl text-stone-700">
-      The RAM website is comming soon!
+    <div>
+      <div className="m-8 text-2xl lg:text-6xl text-stone-700">
+        The RAM website is comming soon!
+      </div>
+      <div className="m-8 text-1xl lg:text-1xl text-stone-700">
+        Richard got everything working in the backend, now he can just build out
+        the website content
+      </div>
     </div>
   );
 }
