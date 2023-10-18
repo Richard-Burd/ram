76848203b6c743da9aacd611d06058fe19b63086
Richard Burd <richardburd@yahoo.com>
2023-10-18 19:20:23 +0000

    <button> tag used for LandingButton component

--- components/LandingButton.js
+++ components/LandingButton.js
@@ -1,12 +1,12 @@
 export default function LandingButton({ title, subTitle, bgColor }) {
   return (
-    <div>
+    <button>
       <div className={`rounded-full m-4 ${bgColor}`}>
         <div className="text-black text-opacity-90 uppercase italic pt-serif-700 mt-8 pl-2 pr-3">
           {title}
         </div>
         <div className="roboto-font-500">{subTitle}</div>
       </div>
-    </div>
+    </button>
   );
 }
