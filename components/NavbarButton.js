43dcf6e32f30de9f98c5ee20830c7e4288dd8d14
Richard Burd <richardburd@yahoo.com>
2023-10-20 23:03:53 +0000

    navbar button animation + glow-shadow

--- components/NavbarButton.js
+++ components/NavbarButton.js
@@ -1,11 +1,23 @@
+import { motion } from "framer-motion";
+
 export default function NavbarButton({ title, bgColor }) {
   return (
-    <div>
-      <div className={`rounded-full m-4 ${bgColor}`}>
+    <motion.div
+      className={`${bgColor} rounded-full m-4 shadow-white`} // Move the bgColor here and add the shadow-white class
+      whileHover={{
+        scale: 1.15,
+        boxShadow: "0 0 90px 5px rgba(255, 255, 255, 0.4)",
+      }}
+      whileTap={{ scale: 0.95 }}
+      transition={{ duration: 0.25 }}
+    >
+      <div className="rounded-full">
+        {" "}
+        {/* Remove the bgColor from here */}
         <div className="text-black text-opacity-90 uppercase italic pt-serif-700 mt-8 pl-2 pr-3">
           {title}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
