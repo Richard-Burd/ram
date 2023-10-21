849fd62e813f881d0925bde2aebb7a791078e8a0
Richard Burd <richardburd@yahoo.com>
2023-10-21 00:32:39 +0000

    transparent logo added to landing page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,15 @@ import LandingButtons from "../components/LandingButtons";
 export default function Home() {
   return (
     <div>
+      <div className="bg-black">
+        <div className="flex justify-center items-center">
+          <img
+            className="w-20 md:w-4/6 m-20"
+            src={`${process.env.NEXT_PUBLIC_ARTICLE_IMAGES_URI_PATH}/ram-logo-trans.png`}
+            alt="RAM Logo"
+          />
+        </div>
+      </div>
       <LandingButtons />
       <div className="m-8 text-2xl lg:text-4xl text-stone-700">
         ♻️ This RAM website is under heavy construction!
