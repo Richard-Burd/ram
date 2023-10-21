a56f590b6a0831cd2b6fe7814a6cb5125fc733a1
Richard Burd <richardburd@yahoo.com>
2023-10-21 00:12:03 +0000

    desktop navbar done

--- components/DesktopNavbar.js
+++ components/DesktopNavbar.js
@@ -3,37 +3,37 @@ import NavbarButton from "./NavbarButton";
 
 export default function DesktopNavbar() {
   return (
-    <nav className="bg-neutral-950 text-white px-2 py-4">
+    <nav className="bg-neutral-950 text-white px-1 py-2 border-y-slate-500 border-b-2">
       <div className="container flex justify-between">
-        <div className="flex-auto flex justify-end">
-          <Link href="/">
+        <div className="flex flex-auto justify-items-stretch items-center">
+          <Link className="min-w-32 flex-grow mx-1" href="/">
             <NavbarButton title="Home" bgColor="bg-homeBg" />
           </Link>
-          <Link href="/about">
+          <Link className="min-w-32 flex-grow mx-1" href="/about">
             <NavbarButton title="About" bgColor="bg-aboutBg" className="ml-4" />
           </Link>
-          <Link href="/studies">
+          <Link className="min-w-32 flex-grow mx-1" href="/studies">
             <NavbarButton
               title="Studies"
               bgColor="bg-studiesBg"
               className="ml-4"
             />
           </Link>
-          <Link href="/outreach">
+          <Link className="min-w-32 flex-grow mx-1" href="/outreach">
             <NavbarButton
               title="Outreach"
               bgColor="bg-outreachBg"
               className="ml-4"
             />
           </Link>
-          <Link href="/successes">
+          <Link className="min-w-32 flex-grow mx-1" href="/successes">
             <NavbarButton
               title="Successes"
               bgColor="bg-successesBg"
               className="ml-4"
             />
           </Link>
-          <Link href="/organizing">
+          <Link className="min-w-32 flex-grow mx-1" href="/organizing">
             <NavbarButton
               title="Organizing"
               bgColor="bg-organizingBg"
--- components/NavbarButton.js
+++ components/NavbarButton.js
@@ -3,18 +3,16 @@ import { motion } from "framer-motion";
 export default function NavbarButton({ title, bgColor }) {
   return (
     <motion.div
-      className={`${bgColor} rounded-full m-4 shadow-white`} // Move the bgColor here and add the shadow-white class
+      className={`${bgColor} rounded-full shadow-white`}
       whileHover={{
-        scale: 1.15,
+        scale: 1.07,
         boxShadow: "0 0 90px 5px rgba(255, 255, 255, 0.4)",
       }}
       whileTap={{ scale: 0.95 }}
       transition={{ duration: 0.25 }}
     >
       <div className="rounded-full">
-        {" "}
-        {/* Remove the bgColor from here */}
-        <div className="text-black text-opacity-90 uppercase italic pt-serif-700 mt-8 pl-2 pr-3">
+        <div className="text-black text-center text-2xl text-opacity-80 uppercase italic pt-serif-700 pl-2 pr-3">
           {title}
         </div>
       </div>
