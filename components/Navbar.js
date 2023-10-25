24fee7c38841c17d7ca99d59bb0f928d9e46baf4
Richard Burd <richardburd@yahoo.com>
2023-10-25 01:25:13 +0000

    added alternate navbar to about page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,17 @@
+import { useRouter, router } from "next/router";
 import DesktopNavbar from "./DesktopNavbar";
 import DesktopNavbarTwo from "./DesktopNavbarTwo";
 import MobileNavbar from "./MobileNavbar";
 
 export default function Navbar() {
+  const router = useRouter();
+  const routesForDesktopNavbarTwo = ["/about"];
+  const isAltIndexPage = routesForDesktopNavbarTwo.includes(router.pathname);
+
   return (
     <>
       <div className="hidden lg:block">
-        <DesktopNavbar />
-        <DesktopNavbarTwo />
+        {isAltIndexPage ? <DesktopNavbarTwo /> : <DesktopNavbar />}
       </div>
       <div className="lg:hidden">
         <MobileNavbar />
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import LandingButtons from "../components/LandingButtons";
+
 export default function About() {
   return (
     <div>
-      <div>This is going to be the About page.</div>
+      <div className="bg-black">
+        <div className="flex justify-center items-center">
+          <img
+            className="w-20 md:w-4/6 m-20"
+            src={`${process.env.NEXT_PUBLIC_ARTICLE_IMAGES_URI_PATH}/ram-logo-trans.png`}
+            alt="RAM Logo"
+          />
+        </div>
+      </div>
+      <LandingButtons />
     </div>
   );
 }
