74f33726c0aa6a2e87f7cb692397ef79250492c3
Richard Burd <richardburd@yahoo.com>
2023-11-07 16:53:27 +0000

    landing page has red background

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import LandingButtons from "../components/LandingButtons";
 export default function Home() {
   return (
     <div>
-      <div className="bg-black">
+      <div
+        className={`bg-gradient-24 to-[#450f08] from-[#000000] from-50% flex border-y-slate-500 border-b-2`}
+      >
         <img
           className="max-w-[85vh] max-h-[90vh] mx-auto py-8"
           src={`${process.env.NEXT_PUBLIC_ARTICLE_IMAGES_URI_PATH}/ram-logo-trans.png`}
