e18031fdebaa219b2f315edb2d2f1a82c82219d6
Richard Burd <richardburd@yahoo.com>
2023-11-14 03:04:35 +0000

    brochure exhibit text complete

--- pages/outreach/brochures.js
+++ pages/outreach/brochures.js
@@ -69,27 +69,93 @@ export default function Brochures() {
                   --David P. Steiner, CEO, Waste Management, Inc.
                 </p>
               </div>
+
               <div id="text-bloc-2" className="mb-24">
                 <p className="mb-4">
-                  <i>RAM</i> showed that loose, treated incinerator ash, used as
-                  construction fill, would make harmful contents more available
-                  to soil life than AAR’s submitted health risk assessment
-                  modeling had assumed.
+                  Landfilling or burning waste causes great damage else- where,
+                  way beyond what a landfill or incinerator does. RAM has always
+                  advocated methods that eliminate landfilling and incineration.
                 </p>
                 <p className="mb-4">
-                  <i>RAM</i> has promoted wet/dry methods, and has worked hard
-                  to explain these in various ways.
+                  <i>RAM</i>AM has graphically communicated basic principles
+                  that are sustainable.
                 </p>
                 <p className="mb-4">
-                  Davidson County began simple wood waste diversion in late
-                  1992, and voluntarily closed its incinerator in 2002 (both
-                  originally proposed by <i>RAM&apos;s</i> John van der Harst),
-                  and has been happy with the results ...
+                  <i>RAM</i> has described high-tech sorting equipment.
                 </p>
                 <p className="mb-4">
-                  ... but to-date, Davidson County has not yet tried wet/dry
-                  methods, and its chosen recycling methods have failed to
-                  achieve promised/desired results.
+                  <i>RAM</i> has described low-tech methods too...
+                </p>
+                <p className="mb-4">
+                  <i>RAM</i> has tried a variety of graphic techniques to
+                  educate, but ultimately nothing will happen unless people
+                  reading these quit sitting on the fence and speak out to their
+                  elected government officeholders!
+                </p>
+              </div>
+
+              <div id="text-bloc-3" className="mb-24">
+                <p className="mb-4">
+                  When <i>RAM</i> began, Davidson and Sumner Counties
+                  incinerated much of their solid waste. <i>RAM&apos;s</i>{" "}
+                  efforts to end this included education sessions, which
+                  explained the Davidson County system clearly.
+                </p>
+                <p className="mb-4">
+                  <i>RAM</i> presented cost/benefit and emissions | comparisons.
+                  Below is one cost graphed and others listed. As the final few
+                  years’ totals were added, <i>RAM&apos;s</i> predictions became
+                  validated.
+                </p>
+                <p className="mb-4">
+                  RAM has graphically shown realities of pollution from
+                  incineration and ash distribution.
+                </p>
+                <p className="mb-4">
+                  Wet/dry pilots <i>RAM</i> participated in never achieved less
+                  than 75% recovery rates.
+                </p>
+                <p className="mb-4">
+                  While per-capita growth of waste in the U.S. has finally
+                  slowed — largely due to exporting it as manufacturing has been
+                  outsourced...
+                </p>
+                <p className="mb-4">
+                  ...Solid waste worldwide is presently projected to double over
+                  the next 15 years if nothing is done to change present trends!
+                  <i>RAM&apos;s</i> plan to totally eliminate waste is needed
+                  now more than ever.
+                </p>
+                <p className="mb-4">
+                  <i>RAM</i> calculated recovery percentages.
+                </p>
+              </div>
+
+              <div id="text-bloc-4" className="mb-24">
+                <p className="mb-4">
+                  Davidson County, like so many other governments, chose
+                  recycling methods advertised by landfill companies. The damage
+                  done has been staggering. <i>RAM</i> has tried many ways to
+                  show the magnitude.
+                </p>
+                <p className="mb-4">
+                  In contrast, <i>RAM</i> constructed pilot demonstrations to
+                  prove performance of wet/dry methods.
+                </p>
+                <p className="mb-4">
+                  Following a pilot in Sumner County, in late 1993, Davidson
+                  County began paying American Ash Recycling. Corp. of TN (AAR)
+                  to distribute treated incinerator ash as loose construction
+                  fill material. <i>RAM</i> objected. <i>RAM&apos;s</i>{" "}
+                  documentation was sought by environmental groups in many
+                  states. Tired of this, AAR sued <i>RAM</i> in early 1997. This
+                  forced <i>RAM</i> to investigate further. This led to
+                  discoveries which ultimately led Davidson County to end both
+                  incineration and its use of AAR.
+                </p>
+                <p className="mb-4">
+                  Over this period, notice how as usage violations became known,
+                  treated ash shipments slowed.
                 </p>
               </div>
             </div>
